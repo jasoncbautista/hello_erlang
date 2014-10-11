3cdee86b4beb255e93f17b7ecfb8763f9c1d8186
Jason <bjason@gmail.com>
2014-10-11 00:19:52 -0700

    assign fxn in erlang

--- simple_funcs.erl
+++ simple_funcs.erl
@@ -2,8 +2,15 @@
 -compile([debug_info, export_all]).
 
 
-
-
+f(X,Y, Callback) ->
+    Callback(X, Y).
 plus(X, Y) ->
     X + Y.
+
+
+
+
+init() ->
+    P = fun simple_funcs:plus/2,
+    f(5,10, P).
 
